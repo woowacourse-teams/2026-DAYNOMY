8eb17b79ac02b2109759f8d98bfe264406cad3a9
Huiyeongkim <huiyeong9619@naver.com>
2026-08-07 14:08:59 +0900

    backend Initial commit

---
+++ backend/src/main/java/org/grit/daynomy/DaynomyApplication.java
@@ -0,0 +1,13 @@
+package org.grit.daynomy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaynomyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaynomyApplication.class, args);
+    }
+
+}
