0bdb1702aaa413f20fb8ea832d2d198bf3db5e98
parkhyomi <phyomi3@naver.com>
2026-08-14 11:14:19 +0900

    feat(search): 헤더에서 검색 페이지 연결

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,14 @@
+import './Header.css'
+
 export function Header() {
   return (
     <header className="daynomy-header">
       <a className="brand" href="/" aria-label="DAYNOMY 홈">
         <span>DAY</span>
         <span>NOMY</span>
+      </a>
+      <a className="search-link" href="/search">
+        검색
       </a>
       <button className="login-button" type="button">
         로그인 / 회원가입
