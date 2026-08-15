2d561525e56223974ec585b99998639dc6961921
Huiyeongkim <huiyeong9619@naver.com>
2026-08-16 04:28:53 +0900

    feat: 뉴스 카테고리 enum 추가

---
+++ backend/src/main/java/org/grit/daynomy/news/domain/Category.java
@@ -0,0 +1,11 @@
+package org.grit.daynomy.news.domain;
+
+public enum Category {
+    POLICY,         // 정부 정책, 규제
+    REAL_ESTATE,    // 부동산
+    STOCK,          // 주식
+    BOND,           // 채권
+    CRYPTO,         // 가상자산
+    COMMODITY,      // 원자재
+    FOREX           // 환율, 외환
+}
