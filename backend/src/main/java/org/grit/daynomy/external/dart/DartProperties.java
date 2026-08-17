a5836f88903fafc25c5afcb642786ffedcf07d49
Huiyeongkim <huiyeong9619@naver.com>
2026-08-17 22:32:10 +0900

    feat: DART API 설정 추가

---
+++ backend/src/main/java/org/grit/daynomy/external/dart/DartProperties.java
@@ -0,0 +1,9 @@
+package org.grit.daynomy.external.dart;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "external.dart")
+public record DartProperties(
+        String apiKey,
+        String baseUrl
+) {}
