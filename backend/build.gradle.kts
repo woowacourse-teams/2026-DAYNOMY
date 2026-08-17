dbd8d06317139df323df2978c76110e9d9cbbbdd
haeseon lee <haeseon4816@gmail.com>
2026-08-18 01:09:33 +0900

    test: H2 기반 테스트 환경 구성

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.security:spring-security-oauth2-jose")
+    testRuntimeOnly("com.h2database:h2")
 }
 
 tasks.withType<Test> {
