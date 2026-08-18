3225683a4916f03dfa92f1e71c9cf1f341c21d89
choiyoung69 <choiyoung609@gmail.com>
2026-08-18 09:40:17 +0900

    feat: 키워드 저장 로직 추가

--- backend/src/main/java/org/grit/daynomy/keyword/service/KeywordService.java
+++ backend/src/main/java/org/grit/daynomy/keyword/service/KeywordService.java
@@ -1,11 +1,14 @@
 package org.grit.daynomy.keyword.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.grit.daynomy.common.BusinessException;
 import org.grit.daynomy.common.ErrorCode;
+import org.grit.daynomy.keyword.domain.Keyword;
 import org.grit.daynomy.keyword.dto.KeywordsResponse;
 import org.grit.daynomy.keyword.parser.KeywordParser;
 import org.grit.daynomy.keyword.repository.NewsKeywordRepository;
+import org.grit.daynomy.news.domain.News;
 import org.grit.daynomy.news.repository.NewsRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +20,12 @@ public class KeywordService {
 
   private final NewsRepository newsRepository;
   private final NewsKeywordRepository newsKeywordRepository;
+
+  @Transactional
+  public void saveKeywords(News news, List<Keyword> keywords) {
+    newsKeywordRepository.saveAll(
+        keywords.stream().map(keyword -> KeywordParser.toEntity(news, keyword)).toList());
+  }
 
   public KeywordsResponse getKeywords(Long newsId) {
     if (!newsRepository.existsById(newsId)) {
