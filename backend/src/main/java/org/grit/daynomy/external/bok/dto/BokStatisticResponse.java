eb29db2e3ea8cab0fd999252123c67981ade185b
choiyoung69 <choiyoung609@gmail.com>
2026-08-20 11:50:45 +0900

    style: ECOS 응답 DTO 포맷 적용

--- backend/src/main/java/org/grit/daynomy/external/bok/dto/BokStatisticResponse.java
+++ backend/src/main/java/org/grit/daynomy/external/bok/dto/BokStatisticResponse.java
@@ -6,11 +6,13 @@ import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record BokStatisticResponse(
-    @JsonProperty("StatisticSearch") StatisticSearch search, @JsonProperty("RESULT") Result result) {
+    @JsonProperty("StatisticSearch") StatisticSearch search,
+    @JsonProperty("RESULT") Result result) {
 
   @JsonIgnoreProperties(ignoreUnknown = true)
   public record StatisticSearch(@JsonProperty("row") List<BokStatisticItem> rows) {}
 
   @JsonIgnoreProperties(ignoreUnknown = true)
-  public record Result(@JsonProperty("CODE") String code, @JsonProperty("MESSAGE") String message) {}
+  public record Result(
+      @JsonProperty("CODE") String code, @JsonProperty("MESSAGE") String message) {}
 }
