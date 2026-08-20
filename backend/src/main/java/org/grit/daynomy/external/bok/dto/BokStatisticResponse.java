a63127c610e7f0dc27bae350c13f17e66b7102e4
choiyoung69 <choiyoung609@gmail.com>
2026-08-20 11:38:10 +0900

    feat: ECOS 오류 응답 DTO 매핑 추가

--- backend/src/main/java/org/grit/daynomy/external/bok/dto/BokStatisticResponse.java
+++ backend/src/main/java/org/grit/daynomy/external/bok/dto/BokStatisticResponse.java
@@ -5,8 +5,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record BokStatisticResponse(@JsonProperty("StatisticSearch") StatisticSearch search) {
+public record BokStatisticResponse(
+    @JsonProperty("StatisticSearch") StatisticSearch search, @JsonProperty("RESULT") Result result) {
 
   @JsonIgnoreProperties(ignoreUnknown = true)
   public record StatisticSearch(@JsonProperty("row") List<BokStatisticItem> rows) {}
+
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  public record Result(@JsonProperty("CODE") String code, @JsonProperty("MESSAGE") String message) {}
 }
