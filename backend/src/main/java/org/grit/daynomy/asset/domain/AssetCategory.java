595a173fa173f687ae78c7298fe93dbd7fe2af5d
choiyoung69 <choiyoung609@gmail.com>
2026-08-23 11:03:47 +0900

    feat: Asset 대신 AssetCategory로 이름 수정

---
+++ backend/src/main/java/org/grit/daynomy/asset/domain/AssetCategory.java
@@ -0,0 +1,14 @@
+package org.grit.daynomy.asset.domain;
+
+public enum AssetCategory {
+  GOLD,
+  STOCK,
+  BOND,
+  REAL_ESTATE,
+  FOREIGN_EXCHANGE,
+  VIRTUAL_ASSET,
+  DEPOSIT_SAVINGS,
+  ETF,
+  PENSION,
+  MOCK
+}
