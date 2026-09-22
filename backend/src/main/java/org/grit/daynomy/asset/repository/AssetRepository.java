d2c31c9321325713c0ea3b8db43c861a9c9f7a1b
choiyoung69 <choiyoung609@gmail.com>
2026-09-22 09:00:43 +0900

    feat: 국내 주식 종목 데이터 모델 추가

--- backend/src/main/java/org/grit/daynomy/asset/repository/AssetRepository.java
+++ backend/src/main/java/org/grit/daynomy/asset/repository/AssetRepository.java
@@ -1,5 +1,6 @@
 package org.grit.daynomy.asset.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.grit.daynomy.asset.domain.Asset;
 import org.grit.daynomy.asset.domain.AssetCategory;
@@ -8,4 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AssetRepository extends JpaRepository<Asset, Long> {
 
   Optional<Asset> findByCategoryAndAssetCode(AssetCategory category, String assetCode);
+
+  List<Asset> findAllByCategory(AssetCategory category);
 }
